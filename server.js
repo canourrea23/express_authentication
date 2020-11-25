f29bbb052a59babdb2a79e7007d79d0190467147
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-11-25 00:49:07 -0500

    redid repo

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
+const session = require('express-session');
+const passport = require('./config/ppConfig');
+const flash = require('connect-flash');
+
+const SECRET_SESSION = process.env.SECRET_SESSION;
+
 const app = express();
 
 app.set('view engine', 'ejs');
